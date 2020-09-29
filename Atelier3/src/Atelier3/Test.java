ca57f5e4b554085339fdc3adcde0bfa6f21a4462
natale2b <natale.mamberti2b@gmail.com>
2020-09-29 10:10:31 +0200

    Atelier3

--- Atelier3/src/Atelier3/Test.java
+++ Atelier3/src/Atelier3/Test.java
@@ -8,11 +8,5 @@ import java.util.GregorianCalendar;
 
 public class Test {
     public static void main(String[] args) {
-        ArrayList<Employe> liste = new ArrayList<Employe>();
-        Adresse adresseBasique = new Adresse(1,"ub","gu","yu");
-        GregorianCalendar dateDeNaissanceGenerique = new GregorianCalendar();
-        dateDeNaissanceGenerique.roll(Calendar.YEAR,-10);
-        Employe e1 = new Employe("leNom1","A",dateDeNaissanceGenerique,adresseBasique,10000., new GregorianCalendar());
-
     }
 }
