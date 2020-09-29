f7515cb8c660ed1917bc082b3b534fcf566820d3
natale2b <natale.mamberti2b@gmail.com>
2020-09-29 17:14:17 +0200

    GeometryObjects

--- Atelier3/src/GeometryObjects/objects2D/Generique2D.java
+++ Atelier3/src/GeometryObjects/objects2D/Generique2D.java
@@ -1,2 +1,14 @@
-package GeometryObjects.objects2D;public class Generique2D {
+package GeometryObjects.objects2D;
+
+import GeometryObjects.Forme;
+
+public abstract class Generique2D extends Forme {
+    public Generique2D(String nom) {
+        super(nom);
+    }
+
+    @Override
+    protected abstract double calculSurface();
+
+    protected abstract double calculPerimetre();
 }
