0dc6c7ffc2101eac8e0df693e1a78ed4e9fc58f9
natale2b <natale.mamberti2b@gmail.com>
2020-09-29 17:16:09 +0200

    GeometryObjects

--- Atelier3/src/GeometryObjects/Forme.java
+++ Atelier3/src/GeometryObjects/Forme.java
@@ -1,2 +1,21 @@
-package GeometryObjects;public class Forme {
+package GeometryObjects;
+
+import java.util.Objects;
+
+public abstract class Forme {
+    private int counter;
+    public final String id;
+
+    public Forme(String nom){
+        counter++;
+        id = nom + "_" + counter;
+    }
+
+    protected abstract double calculSurface();
+    public boolean isBiggerThan(Forme f){
+        if (this.calculSurface()>f.calculSurface()){
+            return true;
+        }
+        return false;
+    }
 }
--- Atelier3/src/GeometryObjects/objects3D/Sphere.java
+++ Atelier3/src/GeometryObjects/objects3D/Sphere.java
@@ -1,2 +1,29 @@
-package GeometryObjects.objects3D;public class Sphere {
+package GeometryObjects.objects3D;
+
+public class Sphere extends Generique3D{
+    private double rayon;
+
+    public Sphere(String nom,double r) {
+        super(nom);
+        rayon = r;
+    }
+
+    @Override
+    public double calculSurface() {
+        return 4 * Math.PI * Math.pow(rayon,2);
+    }
+
+    @Override
+    public double calculVolume() {
+        return 4/3 * Math.PI * Math.pow(rayon,3);
+    }
+
+    @Override
+    public String toString() {
+        return "Sphere{" +
+                "rayon=" + rayon +
+                '}';
+    }
 }
+
+
